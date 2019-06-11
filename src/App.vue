ecf40a08041a48b0f29ec2c623a0ae582570eb8a
Gundulin <cheesit@gmail.com>
2019-06-11 14:26:53 -0600

    initialize

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+<div id="app">
+  <div class="pure-menu">
+    <span class="pure-menu-heading">Photo Bomb</span>
+    <ul class="pure-menu-list">
+      <li class="pure-menu-item">
+        <router-link to="/" class="pure-menu-link">Home</router-link>
+      </li>
+      <li class="pure-menu-item">
+        <router-link to="/mypage" class="pure-menu-link">My Page</router-link>
+      </li>
+    </ul>
+  </div>
+  <div class="content">
+    <router-view />
+  </div>
+</div>
+</template>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
