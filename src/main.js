9a95a6b09f93b5765595d0e1416e0c8cde769056
Gundulin <cheesit@gmail.com>
2019-06-17 20:28:29 -0600

    another update

--- src/main.js
+++ src/main.js
@@ -2,11 +2,33 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import firebase from 'firebase';
 
 Vue.config.productionTip = false
+
+var firebaseConfig = {
+  apiKey: "AIzaSyA1UUspgTof77OW47x9Q6V195OAyMHBezY",
+  authDomain: "cs-260-creative-projects.firebaseapp.com",
+  databaseURL: "https://cs-260-creative-projects.firebaseio.com",
+  projectId: "cs-260-creative-projects",
+  storageBucket: "cs-260-creative-projects.appspot.com",
+  messagingSenderId: "1009512193493",
+  appId: "1:1009512193493:web:f7b72228a8b65a32"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
   store,
   render: h => h(App)
 }).$mount('#app')
+
+firebase.auth().onAuthStateChanged(user => {
+  if(user){
+    store.commit('setUser',user);
+  }
+  else {
+    store.commit('setUser',null);
+  }
+});
--- src/router.js
+++ src/router.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Register from './views/Register.vue'
+import MyPage from './views/MyPage.vue'
+import Login from './views/Login.vue'
 
 Vue.use(Router)
 
@@ -20,6 +23,21 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    },
+    {
+      path: '/mypage',
+      name: 'mypage',
+      component: MyPage,
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: Register
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
     }
   ]
 })
