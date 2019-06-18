4c2cf2202cfe6eb67b644031b094506c46af3e0c
Gundulin <cheesit@gmail.com>
2019-06-18 14:05:46 -0600

    final

--- functions/index.js
+++ functions/index.js
@@ -10,4 +10,23 @@ const app = express();
 
 var db = firebase.firestore();
 
+var itemsRef = db.collection('items');
+
+app.post('/api/items', async (req, res) => {
+    try {
+        let querySnapshot = await itemsRef.get();
+        let numRecords = querySnapshot.docs.length;
+        let item = {
+            id: numRecords + 1,
+            title: req.body.title,
+            path: req.body.path
+        };
+        itemsRef.doc(item.id.toString()).set(item);
+        res.send(item);
+      } catch (error) {
+        // console.log(error);
+        res.sendStatus(500);
+      }
+});
+
 exports.app = functions.https.onRequest(app);
--- src/router.js
+++ src/router.js
@@ -4,6 +4,7 @@ import Home from './views/Home.vue'
 import Register from './views/Register.vue'
 import MyPage from './views/MyPage.vue'
 import Login from './views/Login.vue'
+import Admin from './views/admin.html'
 
 Vue.use(Router)
 
@@ -16,14 +17,14 @@ export default new Router({
       name: 'home',
       component: Home
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    },
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (about.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    // },
     {
       path: '/mypage',
       name: 'mypage',
@@ -38,6 +39,11 @@ export default new Router({
       path: '/login',
       name: 'login',
       component: Login
+    },
+    {
+      path: '/admin',
+      name: 'admin',
+      component: Admin
     }
   ]
 })
